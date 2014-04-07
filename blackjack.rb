938c03e4c1eda09986293e82652a7b9cf8d06c82
Patrick Jones <pmichaeljones@gmail.com>
2014-04-07 14:59:40 -0600

    Finished Hash Values

--- blackjack.rb
+++ blackjack.rb
@@ -18,10 +18,40 @@ def build_deck(numbers, suites, deck = [])
 	#binding.pry
 	numbers.each do |x|
 		suites.each  do |y|
-			deck << x + "-" + y
+			deck << (x + "-" + y)#.to_sym
 		end
 	end
 	return deck
+end
+
+#give the cards a value with ACE exception (1 or 11 depending on value)
+
+def card_values(array)
+	array.inject({}) do |result, element|
+		result[element] = 
+			if element.include?("Ace")
+				1
+			elsif element.include?("2")
+				2
+			elsif element.include?("3")
+				3
+			elsif element.include?("4")
+				4
+			elsif element.include?("5")
+				5
+			elsif element.include?("6")
+				6
+			elsif element.include?("7")
+				7
+			elsif element.include?("8")
+				8
+			elsif element.include?("9")
+				9
+			else #10 jack queen and king are all equal to 10
+				10
+			end
+		result
+	end
 end
 
 puts ">>What is your name?"
@@ -35,11 +65,17 @@ card_names = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Quee
 #suits of cards
 suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
+#instantiate the deck of cards array
 deck_of_cards = build_deck(card_names, suits)
 
-#binding.pry
+#input the deck of cards into the value method to get card values
+card_values_hash = card_values(deck_of_cards)
 
-print deck_of_cards
+puts deck_of_cards
+
+puts card_values_hash
+
+#binding.pry
 
 
 #deal the dealer
