25d1184606ba88bc3e652bc8343b2e2ea6bb90a6
Patrick Jones <pmichaeljones@gmail.com>
2014-04-07 13:04:29 -0600

    Added pseudo-code to start of file

---
+++ blackjack.rb
@@ -0,0 +1,12 @@
+# How the game will work
+
+# 1. Ask the players name and save to variable
+# 2. Deal the dealer two cards and display them. Save sum
+# 3. Deal the player two cards and display them. Save sum
+# 4. Ask player if they want to hit or stay (this is a loop)
+# 5. Once player is done, RETURN out of loop and move to dealer's turn
+# 6. Dealer IS NOT asked to hit or stay. They have set rules for hit & stay
+# 7. Once dealer's card are determined, run evaluation method to find winner.
+# 8. Ask player if they want to play again
+# 9. If YES, start over, if no EXIT.
+
