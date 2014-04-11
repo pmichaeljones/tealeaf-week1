4330732f0714e1eb7bc05614555363d08ecb72b2
Patrick Jones <pmichaeljones@gmail.com>
2014-04-11 14:18:53 -0600

    Initial Commit

--- SolutionBlackJack.rb
+++ SolutionBlackJack.rb
@@ -4,7 +4,7 @@ def calculate_total(cards) # [["Hearts", "3"], ["Clubs", "10"].....]
   total = 0
 
   arr.each do |value|
-    if value == "A"
+    if value == "Ace"
       total += 11
     elsif value.to_i == 0 #jack, queen, king
       total += 10
@@ -14,9 +14,7 @@ def calculate_total(cards) # [["Hearts", "3"], ["Clubs", "10"].....]
   end
 
   arr.select{|e| e == "A"}.count.times do
-    if total > 21
-      total -= 10
-    end
+    total -= 10 if total > 21
   end
 
   total
@@ -55,9 +53,103 @@ mytotal = calculate_total(mycards)
 
 puts "Dealer has: #{dealercards[0]} and a facedown card."
 
-puts "You have: #{mycards[0]} and #{mycards[1]}"
+puts "You have: #{mycards[0]} and #{mycards[1]} for a total of #{mytotal}"
+
+if mytotal == 21
+  puts "Congrats. You hit blackjack!"
+  exit
+end
 
 puts "Do you want to HIT or STAY?"
 
-hit_or_stay = gets.chomp
+while mytotal < 21
+  puts "What would you like to do? 1) Hit or 2) Stay"
+  hit_or_stay = gets.chomp
+
+  if !['1','2'].include?(hit_or_stay)
+    puts "Error: You must enter 1 or 2"
+    next
+  end
+
+  if hit_or_stay == '2'
+    puts "You chose to stay."
+    break
+  end
+
+  #hit
+  if hit_or_stay == '1'
+    new_card = deck.pop
+    mycards << new_card
+    puts "Dealing new cards to player: #{new_card}"
+    mytotal = calculate_total(mycards)
+    puts "Your total is now: #{mytotal}"
+
+    if mytotal == 21
+      puts "Congrats. You got Blackjack!"
+      exit
+    elsif mytotal > 21
+      puts "Sorry, you busted."
+      exit
+    end
+  end
+end
+
+#dealers turn
+
+puts "Dealer shows his cards: #{dealercards[0]} and #{dealercards[1]} for a total of #{dealertotal}"
+
+if dealertotal == 21
+  puts "Dealer hit blackjack. You lose."
+  exit
+end
+
+while dealertotal < 17
+  new_card = deck.pop
+  puts "Dealing new card for dealer...It's a #{new_card}"
+
+  dealercards << new_card
+  dealertotal = calculate_total(dealercards)
+  puts "Dealer total is now: #{dealertotal}"
+
+  if dealertotal == 21
+    puts "Dealer hit blackjack. You lose!"
+    exit
+  elsif dealertotal > 21
+    puts "Dealer busted, you win!"
+    exit
+  end
+end
+
+puts "The dealer has #{dealertotal} and must stay."
+
+puts "Time to see who wins!"
+puts
+
+if dealertotal > mytotal
+  puts "The dealer's ##{dealertotal} beats your #{mytotal}"
+  puts "Dealer wins."
+  exit
+elsif dealertotal < mytotal
+  puts "Your #{mytotal} beats the dealer's #{dealertotal}"
+  puts "You win. Congrats!"
+  exit
+else
+  puts "You both have #{dealertotal}"
+  puts "It's a tie!"
+  exit
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
