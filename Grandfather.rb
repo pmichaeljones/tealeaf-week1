dcf78c5209e2de9483edaabb797f3cef52ee7ab8
Patrick Jones <pmichaeljones@gmail.com>
2014-04-14 10:26:15 -0600

    Added examples from book. Used his answers but retyped

---
+++ Grandfather.rb
@@ -0,0 +1,20 @@
+def grandfather(&block)
+  hour = Time.new.hour
+
+  if hour >= 13
+    hour = hour - 12
+  end
+
+  if hour == 0
+    hour == 12
+  end
+
+  hour.times do
+    block.call
+  end
+
+end
+
+grandfather do
+  puts "Dong"
+end
